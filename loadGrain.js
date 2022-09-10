ab7affac98a6beeebf21b72342c003a14db819e7
Nester44 <nesterov.mail4@gmail.com>
2022-09-10 15:01:54 +0200

    Fix typos

--- loadGrain.js
+++ loadGrain.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const loadGrain = (levels) => {
     const length = levels.length;
     if (length < 3) return 0;
@@ -10,11 +11,8 @@ const loadGrain = (levels) => {
     for(let i = 0; i < length - 1; i++) {
         if (levels[i] > right[i]) {
             right[i] = levels[i];
-
         }
-
         right[i+1] = right[i];
-
     }
 
     const left = [];
@@ -24,33 +22,16 @@ const loadGrain = (levels) => {
     for(let i = length - 1; i > 0; i--) {
         if(levels[i] > left[i]) {
             left[i] = levels[i];
-
         }
-        left[i - 1] = left[i]
-
+        left[i - 1] = left[i];
     }
 
-<<<<<<< HEAD
     let volume = 0;
     for(let i = 0; i < length; i++) {
-        let volumeToAdd = Math.min(right[i], left[i]) - levels[i]
-        
-        if (volumeToAdd > 0) volume += volumeToAdd
-=======
-    if (i === levels.length - 1) {
-      rightBorder = i
-      if(levels[rightBorder] === 0) continue;
-      if (levels[rightBorder] < levels[leftBorder]) {
-        result += (rightBorder - leftBorder - 1) * levels[rightBorder] - edge;
-      }
->>>>>>> 6e169ae65b62907c6af52c0fa2e0d5fcceea0ea0
+        let volumeToAdd = Math.min(right[i], left[i]) - levels[i];
+        if (volumeToAdd > 0) volume += volumeToAdd;
     }
-    return volume
+    return volume;
 }
-<<<<<<< HEAD
 
 export default loadGrain
-
-console.log(loadGrain([4, 0, 1, 3]))
-=======
->>>>>>> 6e169ae65b62907c6af52c0fa2e0d5fcceea0ea0
